e0c266f4814a5611dfa67dc526236aa15f0a8382
mlewis2257 <mlang.lewis@gmail.com>
2026-04-16 02:01:34 -0700

    pm2 restart added for sol-ws-monitor

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@ pip install -r requirements.txt -q
 pm2 restart sol-listener
 pm2 restart sol-backfill
 pm2 restart sol-monitor
+if pm2 describe sol-ws-monitor >/dev/null 2>&1; then
+  pm2 restart sol-ws-monitor
+fi
+pm2 save
 echo "Deployed successfully"
